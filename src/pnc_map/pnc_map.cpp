8c89e91a7387e3092a8e9a87eaac6c6ef9141026
Ling-arch <2315526084@qq.com>
2025-12-06 17:28:33 +0800

    init code

---
+++ src/pnc_map/pnc_map.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "pnc_map.h"
+
+void PNCMap::mapInfo() {
+    std::cout << "PNC Map Information" << std::endl;
+}
---
+++ src/pnc_map/pnc_map.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class PNCMap {
+    public:
+    void mapInfo();
+};
